b15d9276cc1db5d1d970041c5caa9b849aaa112c
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-06-01 16:46:29 +0200

    added favicon and styling to splashscreen

--- Launcher/src/app/app.component.ts
+++ Launcher/src/app/app.component.ts
@@ -7,17 +7,7 @@ import { invoke } from "@tauri-apps/api/tauri";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
-
+  
   @HostListener('document:DOMContentLoaded', ['$event'])
   onDomContentLoaded(event: Event) {
     setTimeout(() => invoke('close_splashscreen'), 2000);
